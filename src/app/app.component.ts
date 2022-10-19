142e14cda034207f2ce40bab57a71aea0506963c
himanshuLadva <himanshu@webtheuniverse.com>
2022-10-19 16:27:15 +0530

    start learning ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectBookCollection, selectBooks } from './state/books.selectors';
+import { retrievedBookList, addBook, removeBook } from './state/books.actions';
+import { BooksService } from './book-list/books.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-redux';
+  books$ = this.store.select(selectBooks);
+  bookCollection$ = this.store.select(selectBookCollection);
+
+  onAdd(bookId: string) {
+    this.store.dispatch(addBook({bookId}));
+  }
+
+  onRemove(bookId: string) {
+    this.store.dispatch(removeBook({bookId}));
+  }
+
+  constructor(private bookService: BooksService, private store: Store) {}
+
+  ngOnInit():void {
+    this.bookService.getBook().subscribe((books) => this.store.dispatch(retrievedBookList({books})));
+  }
 }
